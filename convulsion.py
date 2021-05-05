c0e2d0bfb4ab6aa10aeacf86fbb0c664c31fc6f0
Pau <A01701490@itesm.mx>
2021-05-05 12:06:36 -0500

    fix: display correct output

--- convulsion.py
+++ convulsion.py
@@ -1,35 +1,34 @@
 import numpy as np
 
 def multiplicar(matrix, filtro):
-    colMatrix = len(matrix[0])
-    rowMatrix = len(matrix)
-    colFilter = len(filtro[0])
-    rowFilter = len(filtro)
+    rowsMatrix, colsMatrix = matrix.shape
+    rowsFilter, colsFilter = filtro.shape
     result = 0.0
-    for row in matrix:
-        for col in matrix[row]:
+    for row in range(rowsMatrix):
+        for col in range(colsMatrix):
             result += matrix[row, col] * filtro[row, col]
     return result
 
-matrix = [  [1, 2, 3, 4, 5, 6],
-            [7, 8, 9, 10, 11, 12],
-            [0, 0, 1, 16, 17, 18],
-            [0, 1, 0, 7, 23, 24],
-            [1, 7, 6, 5, 4, 3] ]
+matrix = np.array( [[1, 2, 3, 4, 5, 6],
+                    [7, 8, 9, 10, 11, 12],
+                    [0, 0, 1, 16, 17, 18],
+                    [0, 1, 0, 7, 23, 24],
+                    [1, 7, 6, 5, 4, 3] ])
 
-filterMatrix = [[1, 1, 1],
-                [0, 0, 0],
-                [2, 10, 3]]
+filterMatrix = np.array([[1, 1, 1],
+                        [0, 0, 0],
+                        [2, 10, 3]])
 
-convultion = []
 
-colsMatrix = len(matrix[0])
-rowsMatrix = len(matrix)
-colsFilter = len(filterMatrix[0])
-rowsFilter = len(filterMatrix)
+rowsMatrix, colsMatrix = matrix.shape
+rowsFilter, colsFilter = filterMatrix.shape
+x = rowsMatrix - rowsFilter + 1
+y = colsMatrix - colsFilter + 1
 
-for row in range(rowsMatrix):
-    for col in range(colsMatrix):
-        
-        multiplicar(matrix[row : row + rowsFilter, col:col + colsFilter], filterMatrix)
+output = np.zeros((x,y))
 
+for row in range(x):
+    for col in range(y):
+        output[row,col] = multiplicar(matrix[row : row + rowsFilter, col:col + colsFilter], filterMatrix)
+
+print(output)
