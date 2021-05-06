22ac8b38fb7c5842f01a7c7de451addebd074ad4
Pau <A01701490@itesm.mx>
2021-05-06 09:39:09 -0500

    feat: add padding to matrix

--- convulsion.py
+++ convulsion.py
@@ -9,6 +9,11 @@ def multiplicar(matrix, filtro):
             result += matrix[row, col] * filtro[row, col]
     return result
 
+def padding(padding, matrix):
+    paddedMatrix = np.zeros((matrix.shape[0]+padding*2, matrix.shape[1]+padding*2))
+    paddedMatrix[ padding : paddedMatrix.shape[0]-padding, padding : paddedMatrix.shape[1] - padding] = matrix
+    return paddedMatrix
+
 matrix = np.array( [[1, 2, 3, 4, 5, 6],
                     [7, 8, 9, 10, 11, 12],
                     [0, 0, 1, 16, 17, 18],
@@ -17,7 +22,7 @@ matrix = np.array( [[1, 2, 3, 4, 5, 6],
 
 filterMatrix = np.array([[1, 1, 1],
                         [0, 0, 0],
-                        [2, 10, 3]])
+                        [2,  10, 3]])
 
 
 rowsMatrix, colsMatrix = matrix.shape
@@ -31,4 +36,5 @@ for row in range(x):
     for col in range(y):
         output[row,col] = multiplicar(matrix[row : row + rowsFilter, col:col + colsFilter], filterMatrix)
 
-print(output)
+#print(output)
+#print(padding(2, matrix))
